e785a1837e4133a7e173b5544762194dbc271d18
Efe Öge <efeoge@gmail.com>
2020-11-03 12:15:16 +0300

    Static files

--- slacker/settings/production.py
+++ slacker/settings/production.py
@@ -31,3 +31,16 @@ sentry_sdk.init(
     integrations=[DjangoIntegration()],
     send_default_pii=True,
 )
+
+
+# django-storages
+#
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_FILE_OVERWRITE = False
+AWS_STORAGE_BUCKET_NAME = "mokuslacker"
+ROOT_MEDIA_URL = 'https://{}.s3.amazonaws.com/'.format(AWS_STORAGE_BUCKET_NAME)
+MEDIA_URL = ROOT_MEDIA_URL + 'media/'
+STATIC_URL = ROOT_MEDIA_URL + 'static/'
+AWS_ACCESS_KEY_ID = secrets.AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY = secrets.AWS_SECRET_ACCESS_KEY
